5f9aed50ccd59ecdbc127dcec5104f68e29c0529
liyuanhong <908963295@qq.com>
2018-12-25 00:40:43 +0800

    添加了路由文件的案例

--- nodejs/route_server.js
+++ nodejs/route_server.js
@@ -1,6 +1,29 @@
 /********************************************
         搭建一个简单的nodejs服务器
          通过路由来跳转不同的页面
+   产考地址：http://www.runoob.com/nodejs/nodejs-router.html
  ********************************************/
 
-http = require("http");
+var http = require("http");
+var url = require("url");
+var router = require("./router/router")
+var fs = require('fs');
+
+
+function start(router){
+    var onRequest = function(req,res){
+        var pathname = url.parse(req.url).pathname;
+
+        if(pathname != "/favicon.ico"){
+            var path = router.router(pathname);
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(fs.readFileSync(path));
+            res.end();
+        }
+    }
+
+    http.createServer(onRequest).listen(8080);
+}
+
+start(router);
--- nodejs/router/router.js
+++ nodejs/router/router.js
@@ -2,6 +2,30 @@
            定义一个路由文件
  *************************************/
 
-function route(pathname){
+var fs = require('fs');
 
+
+/*
+首先判断路径或文件是否存在，如果不存在定位到404
+如果存在，这判断是否为目录，如果是目定位到index，如果不是定位到404
+ */
+function router(pathname){
+    //可以在这里添加路由条件
+    var path = __dirname + "/.." + pathname;
+
+    if(fs.existsSync(path)){
+        console.log(fs.statSync(path).isDirectory());
+        if(fs.statSync(path).isDirectory()){
+            path = __dirname + "/index.html"
+        }else{
+            path = __dirname + "/404.html"
+        }
+    }else{
+        path = __dirname + "/404.html"
+    }
+    //path = __dirname + "/.." + "/router/hello.html"
+    console.log("路由到了这里：" + path);
+    return path;
 }
+
+exports.router = router;
